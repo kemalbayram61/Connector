1d3c8e0ee0309f27c594c8a4f0f78116eeec86d3
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-06-13 18:42:16 +0300

    connector update

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+from interfaces.HTTPMethods         import HTTPMethods
+from objects.Application            import Application
+from objects.HTTPRequestConnector   import HTTPRequestConnector
+from objects.Connector              import Connector
+from objects.KNode                  import KNode
+from objects.MongoConnector         import MongoConnector
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    application           = Application("1","Application1", "Application1")
+    #uRule
+    #uruleMongo            = MongoConnector("mongodb+srv:","Database", "Collection")
+    #uruleMongoCon         = Connector("1", "uruleConnector", "uruleConnector", "db")
+    #uruleMongoCon.content = uruleMongo
+    #uruleNode             = KNode(uruleMongoCon)
+    #application.connectorList.append(uruleNode)
+    #get
+    getConnectorContent  = HTTPRequestConnector("https://w3schools.com", HTTPMethods.GET)
+    getConnector         = Connector("2", "findevConnector", "findevConnector", "db")
+    getConnector.content = getConnectorContent
+    getNode              = KNode(getConnector)
+    application.connectorList.append(getNode)
+    #post
+    postData             = {"name": "kemal", "code": 61}
+    postConnectorContent  = HTTPRequestConnector("https://w3schools.com", HTTPMethods.POST, postData)
+    postConnector         = Connector("2", "findevConnector", "findevConnector", "db")
+    postConnector.content = postConnectorContent
+    postNode              = KNode(postConnector)
+    application.connectorList.append(postNode)
+    application.checkConnectors()
+    application.runConnectors()
